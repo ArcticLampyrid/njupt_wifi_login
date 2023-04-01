02994dd63b921481e6c7ec3b7773650e7b73ddea
NKID00 <this@nkid00.name>
2023-04-01 20:46:48 +0800

    feat: trigger network changed by new route

--- core/src/network_changed/linux.rs
+++ core/src/network_changed/linux.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use futures::stream::StreamExt;
 use netlink_packet_core::NetlinkPayload::InnerMessage;
-use netlink_packet_route::{rtnl::address::nlas::Nla::Address, RtnlMessage::NewAddress};
+use netlink_packet_route::{route::Nla::Gateway, RtnlMessage::NewRoute};
 use netlink_sys::{AsyncSocket, SocketAddr};
 use rtnetlink::new_connection;
 use tokio::sync::mpsc::{self, error::TrySendError};
@@ -15,17 +15,24 @@ impl NetworkChangedListener {
 
     pub fn listen(&self) -> Result<mpsc::Receiver<()>> {
         let (mut conn, mut _handle, mut messages) = new_connection()?;
-        let addr = SocketAddr::new(0, libc::RTMGRP_IPV4_IFADDR.try_into().unwrap());
+        let addr = SocketAddr::new(0, (libc::RTMGRP_IPV4_ROUTE).try_into().unwrap());
         conn.socket_mut().socket_mut().bind(&addr)?;
         let (tx, rx) = mpsc::channel(1);
         tokio::spawn(conn);
         tokio::spawn(async move {
             while let Some((message, _)) = messages.next().await {
-                if let InnerMessage(NewAddress(_)) = message.payload {
-                    println!("new addr!!");
-                    if let Err(TrySendError::Closed(_)) = tx.try_send(()) {
-                        break;
+                match message.payload {
+                    InnerMessage(NewRoute(message)) => {
+                        for attr in message.nlas {
+                            if let Gateway(_) = attr {
+                                println!("new route!!");
+                                if let Err(TrySendError::Closed(_)) = tx.try_send(()) {
+                                    break;
+                                }
+                            }
+                        }
                     }
+                    _ => {}
                 }
             }
             drop(tx);
