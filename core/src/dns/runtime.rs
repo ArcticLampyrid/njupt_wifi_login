999a44d23c54e07e8d4604c85a20fce10d7f04d1
ArcticLampyrid <ArcticLampyrid@outlook.com>
2024-05-17 20:08:47 +0800

    fix: potential address family inconsistency

--- core/src/dns/runtime.rs
+++ core/src/dns/runtime.rs
@@ -2,7 +2,10 @@ use futures_util::Future;
 use hickory_proto::iocompat::AsyncIoTokioAsStd;
 use hickory_resolver::{name_server::RuntimeProvider, proto::TokioTime, TokioHandle};
 use network_interface::{NetworkInterface, NetworkInterfaceConfig};
-use std::{net::SocketAddr, pin::Pin};
+use std::{
+    net::{IpAddr, SocketAddr},
+    pin::Pin,
+};
 use tokio::{
     io,
     net::{TcpSocket as TokioTcpSocket, TcpStream as TokioTcpStream, UdpSocket as TokioUdpSocket},
@@ -23,7 +26,27 @@ impl BindableTokioRuntimeProvider {
     }
 }
 
-fn get_bind_addr(interface: &str) -> io::Result<SocketAddr> {
+enum AddressFamilyPreference {
+    V4,
+    V6,
+}
+
+impl AddressFamilyPreference {
+    pub fn for_addr(addr: IpAddr) -> Self {
+        match addr {
+            IpAddr::V4(_) => AddressFamilyPreference::V4,
+            IpAddr::V6(_) => AddressFamilyPreference::V6,
+        }
+    }
+    pub fn is_preferred(&self, addr: IpAddr) -> bool {
+        match self {
+            AddressFamilyPreference::V4 => addr.is_ipv4(),
+            AddressFamilyPreference::V6 => addr.is_ipv6(),
+        }
+    }
+}
+
+fn get_bind_addr(interface: &str, preference: AddressFamilyPreference) -> io::Result<SocketAddr> {
     let interfaces = match NetworkInterface::show() {
         Ok(interfaces) => interfaces,
         Err(e) => return Err(io::Error::new(io::ErrorKind::Other, e)),
@@ -36,10 +59,12 @@ fn get_bind_addr(interface: &str) -> io::Result<SocketAddr> {
         ));
     }
     let interface_info = interface_info.unwrap();
-    let ip_addr = interface_info.addr.iter().find_map(|x| match x {
-        // prefer ipv4
-        network_interface::Addr::V4(_) => Some(x.ip()),
-        _ => None,
+    let ip_addr = interface_info.addr.iter().find_map(|x| {
+        if preference.is_preferred(x.ip()) {
+            Some(x.ip())
+        } else {
+            None
+        }
     });
     let ip_addr = ip_addr.or_else(|| interface_info.addr.first().map(|x| x.ip()));
     if let Some(ip_addr) = ip_addr {
@@ -78,7 +103,10 @@ impl RuntimeProvider for BindableTokioRuntimeProvider {
                 target_os = "linux"
             ))) {
                 if let Some(interface) = interface.as_ref() {
-                    let bind_addr = get_bind_addr(interface)?;
+                    let bind_addr = get_bind_addr(
+                        interface,
+                        AddressFamilyPreference::for_addr(server_addr.ip()),
+                    )?;
                     socket.bind(bind_addr)?;
                 }
             }
@@ -91,7 +119,7 @@ impl RuntimeProvider for BindableTokioRuntimeProvider {
     fn bind_udp(
         &self,
         local_addr: SocketAddr,
-        _server_addr: SocketAddr,
+        server_addr: SocketAddr,
     ) -> Pin<Box<dyn Send + Future<Output = io::Result<Self::Udp>>>> {
         let interface = self.interface.clone();
         Box::pin(async move {
@@ -103,7 +131,10 @@ impl RuntimeProvider for BindableTokioRuntimeProvider {
                 local_addr
             } else {
                 match interface.as_ref() {
-                    Some(interface) => get_bind_addr(interface)?,
+                    Some(interface) => get_bind_addr(
+                        interface,
+                        AddressFamilyPreference::for_addr(server_addr.ip()),
+                    )?,
                     None => local_addr,
                 }
             };
